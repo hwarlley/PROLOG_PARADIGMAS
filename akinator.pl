4cbd3468b1e0d915a454e45a0ca98632f2ffaf60
victorcabeceira <victorfgcabeceira@gmail.com>
2014-05-12 19:27:13 -0300

    Criação de Pessoa. Hipotético

---
+++ akinator.pl
@@ -0,0 +1,7 @@
+pessoa(victor,loiro,curto,azul,alto,5_semestre,software)
+pessoa(joao,preto,curto,castanho,alto,5_semestre,software)
+pessoa(caio,preto,longo,castanho,medio,5_semestre,software)
+pessoa(leandro,preto,curto,castanho,medio,8_semestre,software)
+
+
+pessoa(nome,cor_cabelo,tamanho_cabelo,cor_olho,estatura,semestre,curso)
